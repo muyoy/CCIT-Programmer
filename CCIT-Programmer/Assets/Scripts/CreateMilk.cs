e5404f333c23f5400272898d1967e19afb1af056
muyoy <48443092+muyoy@users.noreply.github.com>
2020-10-16 17:58:17 +0900

    Raycast_test

--- CCIT-Programmer/Assets/Scripts/CreateMilk.cs
+++ CCIT-Programmer/Assets/Scripts/CreateMilk.cs
@@ -6,31 +6,36 @@ public class CreateMilk : MonoBehaviour
 {
 
     public GameObject milk_prefab;
-    private Vector3 pos;
     public float power;
+
     private float cooltime;
 
     private void Start()
     {
-        pos = transform.position; 
-    }
-    private void Update()
-    {
-        cooltime += Time.deltaTime;
-        if(cooltime > 5.0f)
-        {
-            cooltime = 0.0f;
-            Create(); 
-        }
+        StartCoroutine(Milk());
     }
 
-    private void Create()
+    public void Create()
     {
-        GameObject milk = Instantiate(milk_prefab, pos, Quaternion.identity);
+        GameObject milk = Instantiate(milk_prefab, transform.position, Quaternion.identity);
         milk.name = "Milk";
         float x = Random.Range(-1.0f, 1.0f);
         float z = Random.Range( 1.0f, 1.5f);
         milk.GetComponent<Rigidbody>().AddForce(new Vector3(x, 1.0f * power, z), ForceMode.Impulse);
         cooltime = 0.0f;
+    }
+
+    private IEnumerator Milk()
+    {
+        while(true)
+        {
+            cooltime += Time.deltaTime;
+            if (cooltime > 5.0f)
+            {
+                cooltime = 0.0f;
+                Create();
+            }
+            yield return null;
+        }
     }
 }
---
+++ CCIT-Programmer/Assets/Scripts/Tower_Attack.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower_Attack : MonoBehaviour
+{
+
+
+}
+//Debug.DrawRay(transform.position, Vector3.forward * 25, Color.red);
+//RaycastHit hit;
+//Ray ray = new Ray(transform.position, Vector3.forward);
+//int layer_mask = LayerMask.GetMask("Monster");
+//if(Physics.Raycast(ray, out hit, Mathf.Infinity, layer_mask))
+//{
+//    Debug.Log(hit.transform.name);
+//}
