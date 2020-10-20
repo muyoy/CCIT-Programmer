dc08446c7c9d3aea11d400d952aa07d7478f87dc
muyoy <48443092+muyoy@users.noreply.github.com>
2020-10-20 18:43:50 +0900

    monster q

--- CCIT-Programmer/Assets/Scripts/BattleManager.cs
+++ CCIT-Programmer/Assets/Scripts/BattleManager.cs
@@ -4,20 +4,32 @@ using UnityEngine;
 
 public class BattleManager : MonoBehaviour
 {
-    public bool[,] tower_check_grid = new bool[5,9];
-    public int[,] tower_grid = new int[5,9];
+    public bool[,] tower_check_grid = new bool[5, 9];
+    public int[,] tower_grid = new int[5, 9];
+    public Vector3[] line_start = new Vector3[5];
+
+    Queue<GameObject> monsters_0 = new Queue<GameObject>();
+    Queue<GameObject> monsters_1 = new Queue<GameObject>();
+    Queue<GameObject> monsters_2 = new Queue<GameObject>();
+    Queue<GameObject> monsters_3 = new Queue<GameObject>();
+    Queue<GameObject> monsters_4 = new Queue<GameObject>();
+
+
     public void Awake()
     {
+
         for(int i = 0; i < 5; i++)
         {
             for (int j = 0; j < 9; j++)
             {
                 tower_check_grid[i, j] = false;
+                tower_grid[i, j] = i;
             }
         }
+
     }
 
-    bool Tower_Check(GameObject tower)
+    public bool Tower_Check(GameObject tower)
     {
         int x, z;
         x = (int)tower.gameObject.transform.localPosition.x;
@@ -33,6 +45,36 @@ public class BattleManager : MonoBehaviour
             return true;
         }
     }
+
+    public void SetMonsterLine(GameObject monster)
+    {
+        int line = Random.Range(0, 4);
+
+        switch(line)
+        {
+            case 0:
+                monsters_0.Enqueue(monster);
+                monster.transform.position = line_start[0];
+                break;
+            case 1:
+                monsters_1.Enqueue(monster);
+                break;
+            case 2:
+                monsters_2.Enqueue(monster);
+                break;
+            case 3:
+                monsters_3.Enqueue(monster);
+                break;
+            case 4:
+                monsters_4.Enqueue(monster);
+                break;
+        }
+    }
+
+    //public GameObject SetTargetMonster(int line)
+    //{
+
+    //}
 
     private void Update()
     {
