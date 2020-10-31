b45773e01d6977f507c827dfb72c9cbb79213873
tpdms025 <timepark25@naver.com>
2020-10-31 15:58:58 +0900

    201031 백업

---
+++ CCIT-Programmer/Assets/Scripts/Product.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Product : MonoBehaviour
+{
+    [SerializeField] private float speed = 1.0f;
+    [SerializeField] private bool isGet = false;
+    public bool IsGet
+    {
+        get { return isGet; }
+        set
+        {
+            isGet = value;
+            PickUp();
+        }
+    }
+
+    private void Awake()
+    {
+        StartCoroutine(Rotate());
+    }
+    private IEnumerator Rotate()
+    {
+        while (!isGet)
+        {
+            transform.Rotate(0, 90 * Time.deltaTime * speed, 0);
+            yield return null;
+        }
+    }
+    private void PickUp()
+    {
+        isGet = true;
+
+        //TODO:
+        //Particle
+
+
+    }
+
+
+}
